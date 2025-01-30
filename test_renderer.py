dbc4a019e0355e6d39a1c15fd043a2078cdfb293
psy777 <calebehansen@gmail.com>
2025-01-29 19:38:19 -0600

    Asset and kifu support

--- test_renderer.py
+++ test_renderer.py
@@ -1,8 +1,25 @@
 from rust_sgf_renderer import render_sgf
 
+def render(sgf_content, **kwargs):
+    defaults = {
+        "output_path": "output.png",
+        "theme": "paper",
+        "board_width": 19,
+        "board_height": 19,
+    }
+    # Merge defaults with user overrides
+    final = {**defaults, **kwargs}
+
+    render_sgf(
+        sgf_content,
+        final["output_path"],
+        final["theme"],
+        final["board_width"],
+        final["board_height"],
+    )
+
 # Read the SGF content from an sgf file called game_4.sgf
 with open("game_4.sgf", "r", encoding="utf-8") as file:
     sgf_content = file.read()
 
-# Render the SGF to a PNG file called output.png
-render_sgf(sgf_content, "output.png")
+render(sgf_content)
